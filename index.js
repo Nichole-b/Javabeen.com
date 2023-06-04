fdd4762aa7ff92f5de6594be16501ba466dec9fd
Nichole-b <134222768+Nichole-b@users.noreply.github.com>
2023-06-04 17:53:40 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+const username= document.getElementById('yourname');
+const email= document.getElementById('email');
+
+form.addEventlistener('submit', e => {
+    e.preventDefault();
+
+    validateInputs();
+});
+
+const validateInputs = () => {
+    const yourname = yourname.value.trim();
+    const emailValue = email.value.trim();
+}
+
+
+ 
